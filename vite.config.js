5d4d3f7eb201c452216314d9be6a22a6a905dbf3
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-19 20:52:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,21 @@
-placeholder
+import { defineConfig } from 'vite';
+import preact from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [preact()],
+  build: {
+    lib: {
+      entry: 'src/main.jsx',
+      name: 'PreactViteApp',
+      fileName: (format) => `preact-vite-app.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['preact'],
+      output: {
+        globals: {
+          preact: 'Preact',
+        },
+      },
+    },
+  },
+});
